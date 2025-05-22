9e39077b87bf89ed460b3c4c19fa6cbd6228080e
Tran Tien Dat <ttdat260204@gmail.com>
2025-05-22 22:01:50 +0700

    Xong UI Modal các từ đã nhớ

--- src/pages/PracticeFlashCard/index.js
+++ src/pages/PracticeFlashCard/index.js
@@ -4,8 +4,26 @@ import { IoPlayForwardSharp } from "react-icons/io5";
 import { FaRegFaceGrinBeam } from "react-icons/fa6";
 import { FaRegFaceFrownOpen } from "react-icons/fa6";
 import { FaRegFaceAngry } from "react-icons/fa6";
-import CardPracticeBefore from "../../components/CardPractice";
+import { RiDeleteBin6Line } from "react-icons/ri";
+// import CardPracticeBefore from "../../components/CardPractice";
+import BaseModal from "../../components/BaseModal";
+import { useState } from "react";
+import CardPractice from "../../components/CardPractice";
 export default function PracticeFlashCard() {
+    const [open, setOpen] = useState(false);
+
+    const showModal = () => {
+
+        setOpen(true);
+    };
+
+    // const handleOk = () => {
+    //     setOpen(false);
+    // };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
         <>
             <div className="MainContainer">
@@ -15,7 +33,7 @@ export default function PracticeFlashCard() {
                             <h2 className="PracticeFlashCard__title">Test 1 - ETS 2024</h2>
                             <div className="PracticeFlashCard__actions">
                                 <Button className="PracticeFlashCard__btn PracticeFlashCard__btnViewAll" >Xem tất cả</Button>
-                                <Button className="PracticeFlashCard__btn PracticeFlashCard__btnViewMemory">Các từ đã nhớ</Button>
+                                <Button className="PracticeFlashCard__btn PracticeFlashCard__btnViewMemory" onClick={showModal}>Các từ đã nhớ</Button>
                                 <Button className="PracticeFlashCard__btn PracticeFlashCard__btn--Stop">Dừng học list này</Button>
                             </div>
 
@@ -25,7 +43,7 @@ export default function PracticeFlashCard() {
                     <div className="PracticeFlashCard__Content">
                         <p className="PracticeFlashCard__note">Lưu ý: Bạn nên học tối đa 15 từ mới một ngày. Đây là lượng từ phù hợp giúp bạn có thể ghi nhớ tốt</p>
                         <div className="PracticeFlashCard__listFlashCard">
-                            <CardPracticeBefore />
+                            <CardPractice />
                         </div>
                     </div>
                     <div className="PracticeFlashCard__Footer">
@@ -48,6 +66,22 @@ export default function PracticeFlashCard() {
                     </div>
                 </div>
             </div>
+
+            <BaseModal
+                open={open}
+                onCancel={handleClose}
+                title={
+                    <div style={{ fontSize: 24, fontWeight: 'bold' }}>
+                        Danh sách từ đã ghi nhớ
+                    </div>
+                }
+            >
+                <p className="note">Click icon "Delete" để thêm từ vào lại danh sách ôn tập</p>
+                <div className="Card-remember">
+                    <p className="Card-remember__word">Destination</p>
+                    <RiDeleteBin6Line className="Card-remember__delete" />
+                </div>
+            </BaseModal>
         </>
     )
 }
